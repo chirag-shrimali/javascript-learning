a187c79652dfb0227edb2195fc1bcb11608f415d
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-08-27 01:28:58 +0530

    JavaScript Learning

---
+++ Loops/For Loop/index.js
@@ -0,0 +1,73 @@
+// console.log("Hello , World!!");
+
+// Worst things...
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+/**
+    Loops
+    1. For Loop -- Entry Controlled Loop
+    2. While Loop -- Entry Controlled Loop
+    3. Do While -- Exit Controlled Loop
+
+    For Loop Syntax :
+
+    for(initialization ; condition ; updation/update(increament/decreament))
+    {
+        // for loop block / body / work
+    }
+ */
+
+// for(let i = 1 ; i <= 5 ; i++) // i++ // i += 1 // i = i + 1
+// {
+//     console.log(i);
+// }
+
+// for(let i = 1 ; i <= 100 ; i++) // i++ // i += 1 // i = i + 1
+// {
+//     console.log(i);
+// }
+
+// let i = 1;
+
+// for(; i <= 5 ;)
+// {
+//     console.log(i);
+
+//     i++;
+// }
+
+// --------------------------------------------------------------------------------------------------------
+
+// Break Statements -- Skips the current and next iterations...
+
+// for(let i = 1 ; i <= 5 ; i++)
+// {
+//     if(i === 3) break;
+
+//     console.log(i);
+// }
+
+// --------------------------------------------------------------------------------------------------------
+
+// Continue Statements -- Skips the current iterations...
+
+// for(let i = 1 ; i <= 10 ; i++)
+// {
+//     if(i % 2 !== 0) continue;
+
+//     console.log(i);
+// }
+
+// let no = 5;
+
+// console.log();
+
+// for(let i = 1 ; i <= 10 ; i++)
+// {
+//     console.log(`${no} x ${i} = ${no * i}`);
+// }
