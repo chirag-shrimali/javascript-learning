89536d6b7551094f34329d86e28da7be0dad8451
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-08-27 15:42:52 +0530

    JavaScript Learning

---
+++ Loops/Do While Loop/index.js
@@ -0,0 +1,15 @@
+// console.log("Do While Loop!!");
+
+// WAP to print even and odd numbers from 1 to 10...
+
+let i = 1;
+
+do
+{
+    if(i % 2 == 0)
+        
+    console.log(i);
+
+    i++; // i++ // i += 1 // i = i + 1
+
+}while(i <= 10);
---
+++ Loops/While Loop/index.js
@@ -0,0 +1,12 @@
+console.log("While Loop!!");
+
+// WAP to print 1 to 5 number using while loop...
+
+let i = 1;
+
+while(i <= 5)
+{
+    console.log(i);
+
+    i++; // i++ // i += 1 // i = i + 1
+}
