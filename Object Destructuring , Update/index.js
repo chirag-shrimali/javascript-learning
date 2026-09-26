8a5c7549cecef1ab2b6dba0ac3d8cb6b9ad02d70
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-09-26 16:01:21 +0530

    Object adds , updates , deletes , destructruing , array methods

--- Object Destructuring , Update/index.js
+++ Object Destructuring , Update/index.js
@@ -237,18 +237,18 @@ console.log('\n----- OBJECT DE-STRUCTURING || UPDATING -----\n');
 
 // optional chaining...
 
-let obj = 
-{
-    name : 'Chirag' ,
+// let obj = 
+// {
+//     name : 'Chirag' ,
 
-    address : null ,
-};
+//     address : null ,
+// };
 
-console.log(obj);
+// console.log(obj);
 
 // console.log(obj?.address); // here ? is an optional chaining due to this , we can be safely access the object properties...
 
-console.log(obj.address?.street); // undefined -- optional chaining
+// console.log(obj.address?.street); // undefined -- optional chaining
 
 // console.log(obj.address);
 
@@ -256,10 +256,76 @@ console.log(obj.address?.street); // undefined -- optional chaining
 
 // ------------------------------------------------------------------------------------------------------------------------
 
+//              0   1   2   3   4   5   6   7
 
+// const arr = [1 , 2 , 3 , 4 , 5 , 3 , 5 , 9];
+
+//             -8   -7  -6  -5  -4  -3  -2  -1
+
+// console.log(arr.join('#'));
+
+// console.log(arr.indexOf(3)); // 2
+
+// console.log(arr.lastIndexOf(3)); // 5
+
+// console.log(arr.length);
+
+// console.log(arr.toString());
+
+// start to delete , delete count , 
+
+// delete the elements...
+
+// arr.splice(1 , 4);
+
+// add the elements(the elements at that index shiftimg to the right side)...
+
+// arr.splice(4 , 0 , 100);
+
+// arr.splice(8 , 0 , 50);
+
+// replace the elements (add + delete)...
+
+// arr.splice(1 , 1 , 'C');
+
+// arr.splice(2 , 3 , 'CHI');
+
+// console.log(arr); // [1 , 2 , 'CHI' , 3 , 5 , 9]
+
+// console.log(arr.reverse());
+
+// arr.forEach((val , index) => console.log(index , '-' , val));
+
+// console.log(arr.includes(3)); // true
+// console.log(arr.includes(5)); // true
+// console.log(arr.includes(0)); // false
+
+// console.log(arr.indexOf(3)); // 2
+// console.log(arr.indexOf(5)); // 4
+// console.log(arr.indexOf(0)); // -1
+// console.log(arr.indexOf(-6)); // -1
+// console.log(arr.indexOf(2)); // 1
+
+// console.log(arr.slice(4)); // [5]
+
+// starting --> index thi length sudhi lese...
+
+// starting included , ending excluded...
 
 // ------------------------------------------------------------------------------------------------------------------------
 
+// find() methods / functions -- find() function finds the elements if existed in the array...
 
+// let arr = [5 , 9 , 3 , 6 , 7];
+
+// arr.find((val)=> console.log(val));
+
+// let res = arr.find((val) => {
+//     return val === 7;
+
+//     // return val === 10; // undefined because it is not in the array...
+// });
+
+// console.log(res);
 
 // ------------------------------------------------------------------------------------------------------------------------
